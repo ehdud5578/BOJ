975073a2a7a90646e7b2ae3d4f09e0320fef2ff3
DoYoungPark <51391709+ehdud5578@users.noreply.github.com>
2021-05-11 16:33:52 +0900

    dp

--- src/BOJ/practice_may/BOJ10844.java
+++ src/BOJ/practice_may/BOJ10844.java
@@ -6,15 +6,15 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
 public class BOJ10844 {
-    public static void main(String[] args)throws Exception{
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int n = Integer.parseInt(br.readLine());
-        int[][] dp = new int[n+1][10];
-        for(int i = 1;i<10;i++){
+        int[][] dp = new int[n + 1][10];
+        for (int i = 1; i < 10; i++) {
             dp[1][i] = 1;
         }
-        if(n>=2) {
+        if (n >= 2) {
             for (int i = 2; i <= n; i++) {
                 dp[i][0] = dp[i - 1][1];
                 for (int j = 1; j <= 8; j++) {
@@ -24,8 +24,8 @@ public class BOJ10844 {
             }
         }
         int result = 0;
-        for(int i = 0;i<10;i++){
-            result  = (result + dp[n][i])%1000000000;
+        for (int i = 0; i < 10; i++) {
+            result = (result + dp[n][i]) % 1000000000;
         }
         bw.write(result + " ");
         bw.flush();
